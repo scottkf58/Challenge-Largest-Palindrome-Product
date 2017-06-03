8b86748679d23bf9596ac4d0947331c246ffc020
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-06-03 10:40:56 -1000

    it works

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -9,8 +9,34 @@ module.exports = function(digits){
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
+  var max;
 
-  // do your work here
+
+  if ( digits === 2) {
+    max = 100;
+  }
+
+  if (digits === 3) {
+    max = 1000;
+  }
+
+
+  for (var i = 1; i < max; i++) {
+    for (var j = 1; j < max; j++) {
+      var product = i * j;
+      var reverse = product.toString().split('').reverse().join('');
+
+      if (product == reverse && palindromeNumber < product) {
+        factor_0 = i;
+        console.log(factor_0);
+        factor_1 = j;
+        console.log(factor_1);
+        palindromeNumber = product;
+        console.log(palindromeNumber);
+      }
+
+    }
+  }
 
 
 
